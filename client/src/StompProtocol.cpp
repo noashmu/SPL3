a123910565cda6c646bd5395c7ef47c575252832
noaambar <noaamb@post.bgu.ac.il>
2025-01-13 15:05:55 +0200

    summaryFunction

--- client/src/StompProtocol.cpp
+++ client/src/StompProtocol.cpp
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <fstream>
 
+  
 
     std::string StompProtocol::createConnectFrame(const std::string& host, const std::string& username, const std::string& password){
         std::string frame = "CONNECT\n";
@@ -57,7 +58,6 @@
         frame += "\n"; // Empty line indicating the end of the headers
         frame += '\0'; // Null character to terminate the STOMP frame
 
-        totalReport++;
 
         return frame;
     }
@@ -80,8 +80,64 @@
     void StompProtocol::parseAndStoreEvents(const std::string& jsonFile){
 
     }
+    void StompProtocol::report(const std::string& filePath) {
+        // Parse the events file.
+        names_and_events eventsData = parseEventsFile(filePath);
+        const std::string& channelName = eventsData.channel_name;
+        std::vector<event>& events = eventsData.events;
+
+
+        // Save and send each event.
+        for (const auto& event : events) {
+            saveEvent(channelName, event);
+            createSendFrame(channelName,event);
+
+        }
+        }
+    void StompProtocol::saveEvent(const std::string& channelName, const event& event) {
+
+            eventsByChannelAndUser[username][channelName].push_back(event);
+
+    }
+    // Helper function: Convert epoch to date string
+        std::string StompProtocol::epochToDate(time_t epochTime) {
+            std::ostringstream oss;
+            std::tm* tmPtr = std::localtime(&epochTime);
+            oss << std::put_time(tmPtr, "%d/%m/%y %H:%M");
+            return oss.str();
+        }
+
+    // Helper function: Create a summary from description
+    std::string StompProtocol::createSummary(const std::string& description) {
+        if (description.size() <= 27) {
+            return description;
+        }
+        return description.substr(0, 27) + "...";
+    }
 
     void StompProtocol::saveSummaryToFile(const std::string& channel, const std::string& user, const std::string& outputFile){
+        auto userMap = eventsByChannelAndUser.find(user);
+        auto channelMap = userMap->second.find(channel);
+
+        std::vector<event>& events = channelMap->second;
+        std::sort(events.begin(), events.end(), [](const event& a, const event& b) {
+        if (a.get_date_time() != b.get_date_time()) {
+            return a.get_date_time() < b.get_date_time();
+        }
+        return a.get_name() < b.get_name();
+    });
+        int activeCount = 0;
+        int forcesArrivalCount = 0;
+        for (const auto& event : events) {
+            if (event.get_general_information().at("active") == "true") {
+            activeCount++;
+              }
+             if (event.get_general_information().at("forces_arrival_at_scene") == "true") {
+            forcesArrivalCount++;
+             }
+    }
+
+
         if (!std::filesystem::exists(outputFile)) { //if file is not exist, create it
             std::ofstream file(outputFile);
             if (file.is_open()) {
@@ -96,13 +152,20 @@
         if (file.is_open()) {
             file << "Channel: " << channel << std::endl;
             file << "Stas:"<< std::endl;
-            file << "Total:" << this->totalReport << std::endl;
+            file << "Total:" <<  events.size() << std::endl;
+            file << "active: " << activeCount << "\n";
+            file << "forces arrival at scene: " << forcesArrivalCount << "\n";
+            file << "Event Reports:\n";
+        int reportNumber=0;
+        for (const auto& event : events) {
+             file << "Report_" << reportNumber++ << ":\n";
+             file << "city: " << event.get_city() << "\n";
+             file << "date time: " << epochToDate(event.get_date_time()) << "\n";
+             file << "event name: " << event.get_name() << "\n";
+             file << "summary: " << createSummary(event.get_description()) << "\n";
+    }
 
-
-            ////////////////////////
-            ////////need to continue, need to add data
-            ///////////////////////
-
+    file.close();
 
 
             file.close();
@@ -112,9 +175,8 @@
         }
     }
 
-    StompProtocol::StompProtocol(ConnectionHandler& handler){
-        this->totalReport = 0;
-        //this->connectionHandler = handler;
+    StompProtocol::StompProtocol(){
+
     }
 
     StompProtocol::~StompProtocol(){
